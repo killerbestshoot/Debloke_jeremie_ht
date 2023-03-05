bef24b29f2d08f28d15381cfe43363d24549e739
killerbestshoot <killerbestshoot@gmail.com>
2023-03-05 16:43:47 -0500

    test

--- src/main/java/com/example/debloke_jeremie_ht/HelloController.java
+++ src/main/java/com/example/debloke_jeremie_ht/HelloController.java
@@ -1,14 +1,21 @@
 package com.example.debloke_jeremie_ht;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class HelloController {
+    public TextField username;
+    public PasswordField password;
+    public Button authButton;
     @FXML
     private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void authUser(ActionEvent actionEvent) {
+
+        System.out.println(username.getText()+" "+ password.getText());
     }
 }
